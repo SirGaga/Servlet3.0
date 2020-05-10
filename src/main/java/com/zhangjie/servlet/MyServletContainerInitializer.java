974da2884270f6bfd6b90456da72a44ccab8f62d
SirGaga <214151436@qq.com>
2020-05-11 01:02:17 +0800

    8

--- src/main/java/com/zhangjie/servlet/MyServletContainerInitializer.java
+++ src/main/java/com/zhangjie/servlet/MyServletContainerInitializer.java
@@ -2,9 +2,7 @@ package com.zhangjie.servlet;
 
 import com.zhangjie.service.HelloService;
 
-import javax.servlet.ServletContainerInitializer;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
+import javax.servlet.*;
 import javax.servlet.annotation.HandlesTypes;
 import java.util.Set;
 // 容器启动的时候会将 @HandlesTypes 指定的这个类型下面的子类(实现类，子接口等)传递过来
@@ -15,11 +13,15 @@ public class MyServletContainerInitializer implements ServletContainerInitialize
      * @param set @HandlesTypes 注解中引入的所有的类型的子类
      * @param servletContext 代表当前 Web 应用的 servletContext ，一个 web 应用对应一个 ServletContext
      * @throws ServletException
+     * 使用 ServletContext 注册 Web 组件（Servlet、Filter、Listener）
+     *
      */
     @Override
     public void onStartup(Set<Class<?>> set, ServletContext servletContext) throws ServletException {
         System.out.println("感兴趣的类型：");
         set.forEach(clazz -> System.out.println(clazz.getName()+":"+clazz));
+        // 注册组件
+        ServletRegistration.Dynamic userServlet = servletContext.addServlet("userServlet", new UserServlet());
 
     }
 }
---
+++ src/main/java/com/zhangjie/servlet/UserListener.java
@@ -0,0 +1,20 @@
+package com.zhangjie.servlet;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/**
+ * 监听项目的启动和停止
+ */
+public class UserListener implements ServletContextListener {
+    // 监听 ServletContext 启动初始化
+    @Override
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+        System.out.println("UserListener...contextInitialized...");
+    }
+    // 监听 ServletContext 销毁
+    @Override
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+        System.out.println("UserListener...contextDestroyed...");
+    }
+}
